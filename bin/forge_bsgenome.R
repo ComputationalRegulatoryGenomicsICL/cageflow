c2a2dfe01e0945113b5d2a9ba0e42ab70088fa25
Sviatoslav Sidorov <slava@mammoth.cscdom.csc.mrc.ac.uk>
2024-06-19 14:11:16 +0100

    BSgenome package tree is generated

--- bin/forge_bsgenome.R
+++ bin/forge_bsgenome.R
@@ -7,10 +7,5 @@ forge_seed = args[6]
 seqs_srcdir = args[7]
 genome_name = args[8]
 
-bsgenome_destdir = paste0("BSgenome.custom.", genome_name)
-
-dir.create(file.path(bsgenome_destdir))
-
 forgeBSgenomeDataPkg(forge_seed, 
-                     seqs_srcdir, 
-                     bsgenome_destdir)
+                     seqs_srcdir)
