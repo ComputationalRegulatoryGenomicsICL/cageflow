415a6f56a70b2de83fe77da117880721cf20fd65
Katalin Terezia Ferenc <katalitf@uio.no>
2025-06-23 16:12:13 +0200

    fix path with new subfolder

--- testdata/generate_pe_test_data.sh
+++ testdata/generate_pe_test_data.sh
@@ -50,79 +50,79 @@ gzip prim5_SRR10215486_2.fastq
 rm -r SRR10215487 SRR10215486
 
 # Shuffle full samples and remove originals
-seqkit shuffle -s 42 4somites_SRR10215487_1.fastq.gz -o 4somites_SRR10215487_1_shuff.fastq.gz
+~/seqkit shuffle -s 42 4somites_SRR10215487_1.fastq.gz -o 4somites_SRR10215487_1_shuff.fastq.gz
 # [INFO] 7780967 sequences loaded
 rm 4somites_SRR10215487_1.fastq.gz
-seqkit shuffle -s 42 4somites_SRR10215487_2.fastq.gz -o 4somites_SRR10215487_2_shuff.fastq.gz
+~/seqkit shuffle -s 42 4somites_SRR10215487_2.fastq.gz -o 4somites_SRR10215487_2_shuff.fastq.gz
 # [INFO] 7780967 sequences loaded
 rm 4somites_SRR10215487_2.fastq.gz
-seqkit shuffle -s 42 prim5_SRR10215486_1.fastq.gz -o prim5_SRR10215486_1_shuff.fastq.gz
+~/seqkit shuffle -s 42 prim5_SRR10215486_1.fastq.gz -o prim5_SRR10215486_1_shuff.fastq.gz
 # [INFO] 13565767 sequences loaded
 rm prim5_SRR10215486_1.fastq.gz
-seqkit shuffle -s 42 prim5_SRR10215486_2.fastq.gz -o prim5_SRR10215486_2_shuff.fastq.gz
+~/seqkit shuffle -s 42 prim5_SRR10215486_2.fastq.gz -o prim5_SRR10215486_2_shuff.fastq.gz
 # [INFO] 13565767 sequences loaded
 rm prim5_SRR10215486_2.fastq.gz
 
 # Take a random half of reads (after shuffling) to further subset for lane 1 of sample 1
-seqkit head -n 3890483 4somites_SRR10215487_1_shuff.fastq.gz -o 4somites_SRR10215487_1_shuff_top-half.fastq.gz
-seqkit head -n 3890483 4somites_SRR10215487_2_shuff.fastq.gz -o 4somites_SRR10215487_2_shuff_top-half.fastq.gz
+~/seqkit head -n 3890483 4somites_SRR10215487_1_shuff.fastq.gz -o 4somites_SRR10215487_1_shuff_top-half.fastq.gz
+~/seqkit head -n 3890483 4somites_SRR10215487_2_shuff.fastq.gz -o 4somites_SRR10215487_2_shuff_top-half.fastq.gz
 
 # Take the other half of reads to further subset for lane 2 of sample 1
-seqkit range -r -3890484:-1 4somites_SRR10215487_1_shuff.fastq.gz -o 4somites_SRR10215487_1_shuff_bottom-half.fastq.gz
-seqkit range -r -3890484:-1 4somites_SRR10215487_2_shuff.fastq.gz -o 4somites_SRR10215487_2_shuff_bottom-half.fastq.gz
+~/seqkit range -r -3890484:-1 4somites_SRR10215487_1_shuff.fastq.gz -o 4somites_SRR10215487_1_shuff_bottom-half.fastq.gz
+~/seqkit range -r -3890484:-1 4somites_SRR10215487_2_shuff.fastq.gz -o 4somites_SRR10215487_2_shuff_bottom-half.fastq.gz
 
 # Remove the full sample subset above
 rm 4somites_SRR10215487_1_shuff.fastq.gz
 rm 4somites_SRR10215487_2_shuff.fastq.gz
 
 # Take a random half of reads (after shuffling) to further subset for lane 1 of sample 2
-seqkit head -n 6782883 prim5_SRR10215486_1_shuff.fastq.gz -o prim5_SRR10215486_1_shuff_top-half.fastq.gz
-seqkit head -n 6782883 prim5_SRR10215486_2_shuff.fastq.gz -o prim5_SRR10215486_2_shuff_top-half.fastq.gz
+~/seqkit head -n 6782883 prim5_SRR10215486_1_shuff.fastq.gz -o prim5_SRR10215486_1_shuff_top-half.fastq.gz
+~/seqkit head -n 6782883 prim5_SRR10215486_2_shuff.fastq.gz -o prim5_SRR10215486_2_shuff_top-half.fastq.gz
 
 # Take the other half of reads to further subset for lane 2 of sample 2
-seqkit range -r -6782884:-1 prim5_SRR10215486_1_shuff.fastq.gz -o prim5_SRR10215486_1_shuff_bottom-half.fastq.gz
-seqkit range -r -6782884:-1 prim5_SRR10215486_2_shuff.fastq.gz -o prim5_SRR10215486_2_shuff_bottom-half.fastq.gz
+~/seqkit range -r -6782884:-1 prim5_SRR10215486_1_shuff.fastq.gz -o prim5_SRR10215486_1_shuff_bottom-half.fastq.gz
+~/seqkit range -r -6782884:-1 prim5_SRR10215486_2_shuff.fastq.gz -o prim5_SRR10215486_2_shuff_bottom-half.fastq.gz
 
 # Remove the full sample subset above
 rm prim5_SRR10215486_1_shuff.fastq.gz
 rm prim5_SRR10215486_2_shuff.fastq.gz
 
 # Randomly subsample reads for lane 1 of sample 1
-seqkit sample -p 0.1 -s 42 4somites_SRR10215487_1_shuff_top-half.fastq.gz | seqkit sample -n ${readN} -s 42 -o S10_L001_R1.fastq.gz
-# See https://bioinf.shenwei.me/seqkit/note/#effect-of-random-seed-on-results-of-seqkit-sample 
-# on why seqkit sometimes does not output the exact number of reads that was required
-seqkit sample -p 0.1 -s 42 4somites_SRR10215487_2_shuff_top-half.fastq.gz | seqkit sample -n ${readN} -s 42 -o S10_L001_R2.fastq.gz
+~/seqkit sample -p 0.1 -s 42 4somites_SRR10215487_1_shuff_top-half.fastq.gz | ~/seqkit sample -n ${readN} -s 42 -o S10_L001_R1.fastq.gz
+# See https://bioinf.shenwei.me/~/seqkit/note/#effect-of-random-seed-on-results-of-~/seqkit-sample 
+# on why ~/seqkit sometimes does not output the exact number of reads that was required
+~/seqkit sample -p 0.1 -s 42 4somites_SRR10215487_2_shuff_top-half.fastq.gz | ~/seqkit sample -n ${readN} -s 42 -o S10_L001_R2.fastq.gz
 
 # Remove the subset top half
 rm 4somites_SRR10215487_1_shuff_top-half.fastq.gz
 rm 4somites_SRR10215487_2_shuff_top-half.fastq.gz
 
 # Randomly subsample reads for lane 2 of sample 1
-seqkit sample -p 0.1 -s 42 4somites_SRR10215487_1_shuff_bottom-half.fastq.gz | seqkit sample -n ${readN} -s 42 -o S10_L002_R1.fastq.gz
-seqkit sample -p 0.1 -s 42 4somites_SRR10215487_2_shuff_bottom-half.fastq.gz | seqkit sample -n ${readN} -s 42 -o S10_L002_R2.fastq.gz
+~/seqkit sample -p 0.1 -s 42 4somites_SRR10215487_1_shuff_bottom-half.fastq.gz | ~/seqkit sample -n ${readN} -s 42 -o S10_L002_R1.fastq.gz
+~/seqkit sample -p 0.1 -s 42 4somites_SRR10215487_2_shuff_bottom-half.fastq.gz | ~/seqkit sample -n ${readN} -s 42 -o S10_L002_R2.fastq.gz
 
 # Remove the subset bottom half
 rm 4somites_SRR10215487_1_shuff_bottom-half.fastq.gz
 rm 4somites_SRR10215487_2_shuff_bottom-half.fastq.gz
 
 # Randomly subsample reads for lane 1 of sample 2
-seqkit sample -p 0.1 -s 42 prim5_SRR10215486_1_shuff_top-half.fastq.gz | seqkit sample -n ${readN} -s 42 -o S14_L001_R1.fastq.gz
-seqkit sample -p 0.1 -s 42 prim5_SRR10215486_2_shuff_top-half.fastq.gz | seqkit sample -n ${readN} -s 42 -o S14_L001_R2.fastq.gz
+~/seqkit sample -p 0.1 -s 42 prim5_SRR10215486_1_shuff_top-half.fastq.gz | ~/seqkit sample -n ${readN} -s 42 -o S14_L001_R1.fastq.gz
+~/seqkit sample -p 0.1 -s 42 prim5_SRR10215486_2_shuff_top-half.fastq.gz | ~/seqkit sample -n ${readN} -s 42 -o S14_L001_R2.fastq.gz
 
 # Remove the subset top half
 rm prim5_SRR10215486_1_shuff_top-half.fastq.gz
 rm prim5_SRR10215486_2_shuff_top-half.fastq.gz
 
 # Randomly subsample reads for lane 2 of sample 2
-seqkit sample -p 0.1 -s 42 prim5_SRR10215486_1_shuff_bottom-half.fastq.gz | seqkit sample -n ${readN} -s 42 -o S14_L002_R1.fastq.gz
-seqkit sample -p 0.1 -s 42 prim5_SRR10215486_2_shuff_bottom-half.fastq.gz | seqkit sample -n ${readN} -s 42 -o S14_L002_R2.fastq.gz
+~/seqkit sample -p 0.1 -s 42 prim5_SRR10215486_1_shuff_bottom-half.fastq.gz | ~/seqkit sample -n ${readN} -s 42 -o S14_L002_R1.fastq.gz
+~/seqkit sample -p 0.1 -s 42 prim5_SRR10215486_2_shuff_bottom-half.fastq.gz | ~/seqkit sample -n ${readN} -s 42 -o S14_L002_R2.fastq.gz
 
 # Remove the subset bottom half
 rm prim5_SRR10215486_1_shuff_bottom-half.fastq.gz
 rm prim5_SRR10215486_2_shuff_bottom-half.fastq.gz
 
 # To get the reference genome and its annotation, use the following command:
-cd ../danRer11_genome
+cd ../../danRer11_genome
 
 wget https://hgdownload.soe.ucsc.edu/goldenPath/danRer11/bigZips/danRer11.fa.gz
 gunzip danRer11.fa.gz
