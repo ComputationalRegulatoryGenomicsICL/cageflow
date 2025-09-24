3772760f1abdf93e938edd628b3552479d1eaf2e
Sviatoslav Sidorov <slava@mammoth.cscdom.csc.mrc.ac.uk>
2025-09-24 17:11:52 +0100

    Implemented and tested non-5-prime-G removal for paired-end reads

--- testdata/tests/remove_nong_tests.sh
+++ testdata/tests/remove_nong_tests.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 paramfilespath="/mnt/biggley/home/slava/projects/leancage_dev/customcageq/testdata/tests/"
 
-paramsfiles=("params_yeast_borlin_test_star_windex_wfasta_removenong_se.yaml" \
-             "params_danio_nepal_test_bt2_windex_wfasta_removenong_pe.yaml" \
-             "params_danio_nepal_test_star_windex_wfasta_removenong_pe.yaml" \
+paramsfiles=("params_danio_nepal_test_bt2_windex_wfasta_removenong_pe.yaml" \
              "params_yeast_borlin_test_bt2_windex_wfasta_removenong_se.yaml" )
 
 touch mapping_removenong_tests_log.txt
