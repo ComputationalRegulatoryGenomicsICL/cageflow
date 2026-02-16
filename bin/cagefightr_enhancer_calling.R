a6d9d51e19ca218c4b356a6ec4908fdd68a3a871
Katalin Terezia Ferenc <katalitf@uio.no>
2026-02-16 14:11:39 +0100

    modify to fit dev where it matters

--- bin/cagefightr_enhancer_calling.R
+++ bin/cagefightr_enhancer_calling.R
@@ -113,11 +113,9 @@ true_enhancers <- exclude_enhancers_overlapping_promoters(
     ce=ce)
 print("Enhancers overlapping promoters excluded")
 
-saveRDS(true_enhancers, file = "intermediate_cagerobj/nonTSS_enhancers.rds")
-print("Enhancers excluding promoters (consensus clusters) rds file saved")
-
-save_enhancers_to_bed(enhancers=true_enhancers)
-print("Enhancers saved to BED file")
+# save enhancer expression per sample
+outFileNameSamples <- file.path(
+    "tables", "enhancer_expression_per_sample.tsv")
 
 # annotate enhancers with transcript database information
 tx_annotation_obj <- loadDb(tx_annotation)
@@ -129,70 +127,64 @@ if (length(true_enhancers) > 0) {
     txdb=tx_annotation_obj,
     tssregion_up=tssregion_up,
     tssregion_down=tssregion_down)
+
+  print("Enhancers annotated")
+
+  saveRDS(true_enhancers, file = "intermediate_cagerobj/nonTSS_enhancers.rds")
+  print("Enhancers excluding promoters (consensus clusters) rds file saved")
+
+  print("Saving enhancers to bed file...")
+  save_enhancers_to_bed(enhancers=true_enhancers)
+  print("Enhancers saved to BED file")
+
+  # assign enhancers to samples
+  enhancer_expr_per_sample <- identify_sample_specific_enhancers(
+    true_enhancers=true_enhancers,
+    ce=ce)
+
+  print("Enhancers assigned to samples")
+
+  write.table(
+    enhancer_expr_per_sample,
+    file=outFileNameSamples,
+    quote=FALSE,
+    sep='\t')
+  print("Enhancer expressions per sample saved to file")
+
+  # plot PCA of enhancer expression per sample
+  pca_plot <- plot_pcs(
+    count_matrix=enhancer_expr_per_sample)
+  save_plot(
+    "enhancer_expression_pca_plot.pdf",
+    pca_plot)
+  print("PCA plot of enhancer expression per sample saved")
+
+  # count and plot number of enhancers per sample
+  sample_enhancer_count <- count_number_of_enhancers(
+    enhancer_expr_per_sample=enhancer_expr_per_sample)
+  # function from qc_plots.R
+  enhancer_count_plot <- plot_number_of_tag_clusters(
+    sample_tag_count=sample_enhancer_count,
+    yaxistitle="Number of enhancers per sample",
+    mytitle="Number of enhancers per sample",
+    myfilename="enhancer_count_per_sample")
+  save_plot(
+    "enhancer_count_per_sample_plot.pdf",
+    enhancer_count_plot)
+  print("Enhancer counts plotted")
+
 } else {
     chipannot_empty_plot = make_no_enhancer_plot()
     save_plot(
         "chipseeker_enhancer_annotation_plot.pdf",
         chipannot_empty_plot
     )
-}
-
-print("Enhancers annotated")
-
-if (length(true_enhancers) > 0) {
-    # assign enhancers to samples
-    enhancer_expr_per_sample <- identify_sample_specific_enhancers(
-        true_enhancers=true_enhancers,
-        ce=ce)
-
-    print("Enhancers assigned to samples")
-}
-
-# save enhancer expression per sample
-outFileNameSamples <- file.path(
-    "tables", "enhancer_expression_per_sample.tsv")
-
-if (length(true_enhancers) > 0) {
-    write.table(
-        enhancer_expr_per_sample,
-        file=outFileNameSamples,
-        quote=FALSE,
-        sep='\t')
-    print("Enhancer expressions per sample saved to file")
-} else {
     file.create(outFileNameSamples)
-}
-
-if (length(true_enhancers) > 0) {
-    # plot PCA of enhancer expression per sample
-    pca_plot <- plot_pcs(
-        count_matrix=enhancer_expr_per_sample)
-    save_plot(
-        "enhancer_expression_pca_plot.pdf",
-        pca_plot)
-    print("PCA plot of enhancer expression per sample saved")
-} else {
     pca_empty_plot = make_no_enhancer_plot()
     save_plot(
         "enhancer_expression_pca_plot.pdf",
         pca_empty_plot)
-}
 
-if (length(true_enhancers) > 0) {
-    # count and plot number of enhancers per sample
-    sample_enhancer_count <- count_number_of_enhancers(
-        enhancer_expr_per_sample=enhancer_expr_per_sample)
-    # function from qc_plots.R
-    enhancer_count_plot <- plot_number_of_tag_clusters(
-        sample_tag_count=sample_enhancer_count,
-        yaxistitle="Number of enhancers per sample",
-        mytitle="Number of enhancers per sample",
-        myfilename="enhancer_count_per_sample")
-    save_plot(
-        "enhancer_count_per_sample_plot.pdf",
-        enhancer_count_plot)
-    print("Enhancer counts plotted")
-} else {
     enhancer_count_empty_plot = make_no_enhancer_plot()
     save_plot(
         "enhancer_count_per_sample_plot.pdf",
